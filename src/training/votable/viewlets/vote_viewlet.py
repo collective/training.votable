5c6211eb46ec9d0672aa736bf8cf107eba734964
Katja Süss <k.suess@rohberg.ch>
2022-08-27 13:08:20 +0200

    Rely on permissions.

--- src/training/votable/viewlets/vote_viewlet.py
+++ src/training/votable/viewlets/vote_viewlet.py
@@ -1,31 +1,36 @@
 # -*- coding: utf-8 -*-
 
 from plone.app.layout.viewlets import ViewletBase
-from Products.CMFCore.permissions import ViewManagementScreens
 from Products.CMFCore.utils import getToolByName
 
-from training.votable import DoVotePermission
+from training.votable import (
+    ClearVotesPermission,
+    CanVotePermission,
+    ViewVotesPermission,
+)
 from training.votable.behaviors.votable import IVotable
 
 
 class VoteViewlet(ViewletBase):
     vote = None
-    is_manager = None
+    can_clear_votes = None
+    can_view_votes = None
     can_vote = None
 
     def update(self):
         super(VoteViewlet, self).update()
 
-        if self.vote is None:
+        membership_tool = getToolByName(self.context, "portal_membership")
+        self.can_view_votes = membership_tool.checkPermission(
+            ViewVotesPermission, self.context
+        )
+        self.can_vote = membership_tool.checkPermission(CanVotePermission, self.context)
+        self.can_clear_votes = membership_tool.checkPermission(
+            ClearVotesPermission, self.context
+        )
+
+        if self.can_view_votes:
             self.vote = IVotable(self.context)
-        if self.is_manager is None:
-            membership_tool = getToolByName(self.context, "portal_membership")
-            self.is_manager = membership_tool.checkPermission(
-                ViewManagementScreens, self.context
-            )
-            self.can_vote = membership_tool.checkPermission(
-                DoVotePermission, self.context
-            )
 
     def voted(self):
         return self.vote.already_voted(self.request)
